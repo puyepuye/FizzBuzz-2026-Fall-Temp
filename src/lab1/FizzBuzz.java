0a1490776457e242f7f067800dd133813b47c352
Sataphon Obra <puye53991@gmail.com>
2026-09-15 18:56:35 -0400

    fix the button

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -9,6 +9,7 @@ class FizzBuzz {
 
         for (int i = 1; i <= 100; i++) {
             // adding a comment
+            // adfadsf
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
