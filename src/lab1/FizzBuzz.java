0bc1b31abd42431a1f21f7cfb16c7edc88924986
Sataphon Obra <puye53991@gmail.com>
2026-09-15 18:57:56 -0400

    great message

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -13,7 +13,7 @@ class FizzBuzz {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
-
+            ///asdfdfsdfdsf
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
 
